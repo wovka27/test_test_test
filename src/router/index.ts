169da9961bea58960dd341537a31fe2a3067ce55
Vladimir Demyanchuk <wovka.pb@yandex.ru>
2025-03-26 20:19:27 +0500

    change router

--- src/router/index.ts
+++ src/router/index.ts
@@ -14,16 +14,16 @@ const router = createRouter({
   routes: [
     { path: '/', redirect: '/demo-1' },
     { path: '/:catchAll(.*)', redirect: '/' },
-    { path: '/demo-1', component: DemoListView },
+    { path: '/demo-1', name: 'demo-1', component: DemoListView },
     { path: '/demo-1/:uid', component: DemoEditorView },
 
-    { path: '/demo-3', component: Demo3ListView },
+    { path: '/demo-3', name: 'demo-3', component: Demo3ListView },
     { path: '/demo-3/:uid', component: Demo3EditorView },
 
-    { path: '/demo-4', component: Demo4ListView },
+    { path: '/demo-4', name: 'demo-4', component: Demo4ListView },
     { path: '/demo-4/:uid', component: Demo4EditorView },
 
-    { path: '/demo-5', component: Demo5ListView },
+    { path: '/demo-5', name: 'demo-5', component: Demo5ListView },
     { path: '/demo-5/:uid', component: Demo5EditorView }
   ]
 })
