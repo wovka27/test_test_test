4af9b660da9609ebe5e1b61ceace0e55913e4ff2
Vladimir Demyanchuk <wovka.pb@yandex.ru>
2025-03-27 13:37:39 +0500

    change main.yml

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
+  base: '/wovka27.github.io/test_test_test',
   build: {
     outDir: 'dist'
   },
